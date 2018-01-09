9fed3f80e641cff53feb065bf37fc146b37990b9
harjindergill\gill <laddivaliant@live.com>
2018-01-09 15:50:45 -0800

    yelp API is setup

---
+++ src/util/yelp.js
@@ -0,0 +1,18 @@
+const apiKey = 'y5KM_fS1g2gnovQnDyXR8FUc6aSXOn2VO_zTjcZnFg-WYmnlnQtl5CbHZhZ8XCUt0pxBrlsOXR5sEl3A5mhd1EBXlCsDXA3GcpOnadqpibujA7hOzyLVeOxxt_hTWnYx';
+
+
+// this object is used to store the functionality 
+// needed to interact with YELP API
+
+var Yelp = {
+
+}
+
+// This method is used to retriev 
+// the search result from YELP API
+
+
+Yelp.serach = function(term, loaction, sortBy){
+	return fetch();
+
+};
