742fe9ee6f8cd46edf51d5557c38b3ecf9aed5ab
harjindergill\gill <laddivaliant@live.com>
2018-01-10 13:08:42 -0800

    working app

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -57,7 +57,7 @@ class SearchBar extends React.Component {
                 {sortByOption}
              </li>);
     });
-  }//
+  }
 
   render() {
     return (
--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,18 +1,42 @@
-const apiKey = 'y5KM_fS1g2gnovQnDyXR8FUc6aSXOn2VO_zTjcZnFg-WYmnlnQtl5CbHZhZ8XCUt0pxBrlsOXR5sEl3A5mhd1EBXlCsDXA3GcpOnadqpibujA7hOzyLVeOxxt_hTWnYx';
+const apiKey = 'APi_goes_here';
 
 
 // this object is used to store the functionality 
 // needed to interact with YELP API
 
-var Yelp = {
-
-}
-
-// This method is used to retriev 
-// the search result from YELP API
+const Yelp = {
+	// This method is used to retriev 
+	// the search result from YELP API
 
 
-Yelp.serach = function(term, loaction, sortBy){
-	return fetch();
+	search(term, location, sortBy){
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+			headers: {
+				 Authorization: `Bearer ${apiKey}`  // this take care of Authorization for browser to acces API
+			}
+		}).then(response => {
+			return response.json();
+		}).then(jsonResponse => {
+			if(jsonResponse.businesses){
+				return jsonResponse.businesses.map(business => ({
+						id: business.id,
+				        imageSrc: business.image_url,
+				        name: business.name,
+				        address: business.location.address1,
+				        city: business.location.city,
+				        state: business.location.state,
+				        zipCode: business.location.zip_code,
+				        category: business.categories[0].title,
+				        rating: business.rating,
+				        reviewCount: business.review_count
+					}));
+			}
+		});
 
+
+	}
 };
+
+
+
+export default Yelp;
